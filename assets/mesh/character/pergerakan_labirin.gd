bfb6c8b3f4ad44c8e94bddfa6f36512dd90190e4
nandi <Nandichanel@gmail.com>
2023-04-24 20:58:55 +0700

    commit ke-3

---
+++ assets/mesh/character/pergerakan_labirin.gd
@@ -0,0 +1,34 @@
+extends KinematicBody
+
+var pergerakan = Vector3.ZERO
+var gravitasi = 0.2
+var kec_jatuh = 0
+onready var joystick = get_parent().get_node("control/Sprite/TouchScreenButton")
+onready var raycast = $SpringArm/RayCast
+
+func _physics_process(_delta):
+
+	rotate_y(joystick.rotate.x)
+	$SpringArm.rotate_x(joystick.rotate.y)
+#	rotation_degrees.z = 0
+#	$SpringArm.rotation.x = clamp($SpringArm.rotation.x,deg2rad(45),deg2rad(-45))
+	
+	pergerakan.x = joystick.get_value().x
+	pergerakan.z = joystick.get_value().y
+	
+	if(!is_on_floor()):
+		kec_jatuh -= gravitasi
+	elif(is_on_floor()):
+		kec_jatuh = 0
+	pergerakan.y += kec_jatuh
+	
+	pergerakan = (pergerakan.x * transform.basis.x) + (pergerakan.z * transform.basis.z)
+	var kecepatan = pergerakan * 20
+	
+	move_and_slide(kecepatan,Vector3.UP)
+	joystick.rotate = Vector2.ZERO
+#	mesh1.get_name()
+	
+func _input(event):
+	if(event is InputEventScreenDrag):
+		$SpringArm.rotation.x = clamp($SpringArm.rotation.x,deg2rad(-45),deg2rad(45))
